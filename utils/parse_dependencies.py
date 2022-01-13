d6c5381a7d856231b1661963257e381529870d91
loganpeng1992 <loganpeng1992@gmail.com>
2022-01-13 00:55:34 -0500

    reparsed conllu from gold tokens

---
+++ utils/parse_dependencies.py
@@ -0,0 +1,115 @@
+import os, io, re
+import argparse
+from glob import glob
+
+import stanza
+from stanza.utils.conll import CoNLL
+
+
+
+def set_up_nlp(has_gold_tokens=True, has_gold_sentences=True, use_onto_trained=True):
+	model_path = {} if not use_onto_trained else {
+		"pos_pretrain_path": "../../../code/stanza-train/stanza/saved_models/pos/zh_ontonotes.pretrain.pt",
+        "pos_model_path" : "../../../code/stanza-train/stanza/saved_models/pos/zh_ontonotes_tagger.pt",
+        "depparse_pretrain_path": "../../../code/stanza-train/stanza/saved_models/depparse/zh_ontonotes.pretrain.pt",
+        "depparse_model_path": "../../../code/stanza-train/stanza/saved_models/depparse/zh_ontonotes_parser.pt"}
+	nlp = stanza.Pipeline("zh", processors='tokenize,pos,lemma,depparse',
+	                           tokenize_pretokenized=has_gold_tokens,
+	                           tokenize_no_ssplit=has_gold_sentences,
+	                           **model_path
+	                           )
+	return nlp
+
+def parse_sentence(nlp, sentence):
+	doc = nlp(sentence)  # Run the pipeline on input text
+	dicts = doc.to_dict()
+	conllu = CoNLL.convert_dict(dicts)
+	tokens = " ".join([x[1] for x in conllu[0]])
+	conllu = "\n".join(['\t'.join(x) for x in conllu[0]])
+	return tokens, conllu
+
+
+def read_txt_file(txt_file):
+	with io.open(txt_file, "r", encoding="utf8") as f_txt:
+		txt_lines = f_txt.read().strip().split("\n")
+	txt_lines = [x for x in txt_lines if not x.startswith("<") or not re.match(r"^\s*$", x)]
+	return txt_lines
+
+def write_parsed_file(output_parse, parsed_file):
+	with io.open(parsed_file, "w", encoding="utf8") as f_parsed:
+		f_parsed.write("\n\n".join(output_parse) + "\n\n")
+
+def write_tokenized_file(output_token, tokenized_file):
+	with io.open(tokenized_file, "w", encoding="utf8") as f_tokenized:
+		f_tokenized.write("\n".join(output_token) + "\n")
+
+def parse_documents(parsed_output_dir, mode="gold_token", use_onto_trained=True, replace_existing = False):
+	# Set up nlp
+	if mode == "gold_token":
+		nlp = set_up_nlp(has_gold_tokens=True, has_gold_sentences=True, use_onto_trained=use_onto_trained)
+	elif mode == "raw_text":
+		nlp = set_up_nlp(has_gold_tokens=False, has_gold_sentences=True, use_onto_trained=use_onto_trained)
+	
+	if mode == "raw_text":
+		input_dir = "../data/raw/"
+	elif mode == "gold_token":
+		input_dir = "../data/tokenized/gold/"
+	
+	# Set up output tokenized directory if parse from raw
+	if mode == "raw_text":
+		tokenized_output_dir = "../data/tokenized/stanza/"
+		if not os.path.isdir(tokenized_output_dir):
+			os.makedirs(tokenized_output_dir)
+
+	# Iterate through documents
+	docs = sorted(list(glob(input_dir + "gum_zh_*.txt", recursive=False)))
+	for doc in docs:
+		basename = os.path.splitext(os.path.basename(doc))[0]
+		parsed_doc = parsed_output_dir + basename + ".conllu"
+		if mode == "raw_text":
+			tokenized_doc = tokenized_output_dir + basename + ".txt"
+		
+		# Replace or not
+		if os.path.exists(parsed_doc) and not replace_existing:
+			print("o Skipping %s since it already exists" % basename)
+			continue
+		else:
+			lines = read_txt_file(doc)
+			tokens_list = []
+			conllu_list = []
+			for line in lines:
+				tokens, conllu = parse_sentence(nlp, line)
+				tokens_list.append(tokens)
+				conllu_list.append(conllu)
+			
+			# Write or assert tokens
+			if mode == "raw_text":
+				write_tokenized_file(tokens_list, tokenized_doc)
+			elif mode == "gold_token":
+				mismatches = [(idx, lines[idx], tokens_list[idx]) for idx in range(len(tokens_list)) if lines[idx]!=tokens_list[idx]]
+				assert tokens_list == lines
+				
+			# Write parsed dependencies
+			write_parsed_file(conllu_list, parsed_doc)
+			
+			print("o Done with parsing: ", basename)
+
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--mode", choices=["raw_text", "gold_token"], default="gold_token")
+	parser.add_argument("--parsed_dir", default="../data/parsed/stanza/")
+	parser.add_argument("--replace_existing", choices=[True, False], default=False)
+	parser.add_argument("--use_onto_trained", choices=[True, False], default=True)
+	args = parser.parse_args()
+	
+	if not os.path.isdir(args.parsed_dir):
+		os.makedirs(args.parsed_dir)
+		
+	parse_documents(parsed_output_dir=args.parsed_dir,
+                    mode=args.mode,
+                    use_onto_trained=args.use_onto_trained,
+                    replace_existing = args.replace_existing)
+	
+	print('o ALL DONE!')
